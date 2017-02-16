ce3d73548a2fe8a83dc5b04b6469ce6a7b091e40
Pierre Laurent <pierre.laurent@reseau.eseo.fr>
2017-02-16 12:14:28 +0100

    data recovered
    we gather and reform the data in the right format to call the printData
    function we already didin the youcanedit.js

--- testsend.js
+++ testsend.js
@@ -1,6 +1,11 @@
-var productName = "nom du produit"
-var specifications = [{"name":"name1","value":"value1"},{"name":"name2","value":"value2"}];
-var sessionId = "16/01/2017 00:42";
+var productName = {"productName" : "nom du produit"}
+var specifications = {"specifications" :[{"name":"name1","value":"value1"},{"name":"name2","value":"value2"},{"name":"name3","value":"value3"}]};
+var sessionId = {"sessionID": "16/01/2017 00:42"};
+
+var specifications2 = [{"name":"name1","value":"value1"},{"name":"name2","value":"value2"},{"name":"name3","value":"value3"}];
+var specLength = Object.keys(specifications2).length
+
+console.log(specLength)
 
 var data = []
 data.push(productName)
@@ -32,7 +37,10 @@ XHR.setRequestHeader("Content-Type", "text/xml");
             if (XHR.status == 200) {
 				var dataResponse = XHR.responseText;
 				var jsonResponse = JSON.parse(dataResponse);
-				console.log(jsonResponse);
+				
+				var productNameCap = recoverProductName(jsonResponse)
+				var specificationsCap = recoverSpecifications(jsonResponse, specLength)
+				var sessionIdCap = recoverSessionId(jsonResponse)
             }
         }
     };
@@ -40,3 +48,42 @@ XHR.setRequestHeader("Content-Type", "text/xml");
 // We just send our FormData object, HTTP headers are set automatically
 var dataSend = JSON.stringify({data});
 XHR.send(dataSend);
+
+
+function recoverDataFromServ(data){
+	console.log(data)
+	console.log(data.DATA[0].PRODUCTNAME);
+	var productNameCap = data.DATA[0].PRODUCTNAME
+
+	var specificationArray = [] 
+	for (var i = 0; i<this.specLength; i++) {
+		var specificationElements = {name:data.DATA[1].SPECIFICATIONS[i].NAME, value: data.DATA[1].SPECIFICATIONS[i].VALUE}
+		specificationArray.push(specificationElements); 
+	};
+	console.log(specificationArray);
+
+	var sessionId = data.DATA[2].SESSIONID
+	console.log(sessionId);
+}
+function recoverProductName(data){
+	var productNameCap = data.DATA[0].PRODUCTNAME
+	console.log(productNameCap);
+	return productNameCap;
+}
+
+function recoverSpecifications(data, specLength){
+	var specificationArray = [] 
+
+	for (var i = 0; i<specLength; i++) {
+		var specificationElements = {name:data.DATA[1].SPECIFICATIONS[i].NAME, value: data.DATA[1].SPECIFICATIONS[i].VALUE}
+		specificationArray.push(specificationElements); 
+	};
+	console.log(specificationArray);
+	return specificationArray;
+}
+
+function recoverSessionId(data){
+	var sessionId = data.DATA[2].SESSIONID
+	console.log(sessionId);
+	return sessionId;
+}
